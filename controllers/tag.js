b923b42eda6de08e0b32c1849c35c13d9abcf6f8
milek agrawal <milekag01@gmail.com>
2020-08-01 07:34:12 +0530

    add user, category and tag model, controller and routes

---
+++ controllers/tag.js
@@ -0,0 +1,63 @@
+const Tag = require('../models/tag');
+const slugify = require('slugify');
+const { errorHandler } = require('../helpers/dbErrorHandler');
+
+// create new tag
+exports.create = (req, res) => {
+    const { name } = req.body;
+    let slug = slugify(name).toLowerCase();
+
+    let tag = new Tag({ name, slug });
+
+    tag.save((err, data) => {
+        if (err) {
+            console.log(err);
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json(data); // dont do this res.json({ tag: data });
+    });
+};
+
+// show all tags
+exports.list = (req, res) => {
+    Tag.find({}).exec((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json(data);
+    });
+};
+
+// read a tag according to slug
+exports.read = (req, res) => {
+    const slug = req.params.slug.toLowerCase();
+
+    Tag.findOne({ slug }).exec((err, tag) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Tag not found'
+            });
+        }
+        res.json(tag);
+    });
+};
+
+// remove a tag
+exports.remove = (req, res) => {
+    const slug = req.params.slug.toLowerCase();
+
+    Tag.findOneAndRemove({ slug }).exec((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            message: 'Tag deleted successfully'
+        });
+    });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -10,10 +10,4 @@ router.post('/signup', userSignupValidator, runValidation, signup);
 router.post('/signin', userSigninValidator, runValidation, signin);
 router.get('/signout', signout);
 
-// test
-router.get('/secret', requireSignin, (req, res) => {
-    res.json({
-        message: 'you have access to secret page'
-    });
-});
 module.exports = router;
