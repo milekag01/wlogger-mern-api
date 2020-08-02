3e95a89b4848eb49807be7a77d54e9d1da5d12c0
milek agrawal <milekag01@gmail.com>
2020-08-02 21:41:18 +0530

    add tag and category blog fetch

--- controllers/tag.js
+++ controllers/tag.js
@@ -1,4 +1,5 @@
 const Tag = require('../models/tag');
+const Blog = require('../models/blog');
 const slugify = require('slugify');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
@@ -33,6 +34,20 @@ exports.list = (req, res) => {
 };
 
 // read a tag according to slug
+// exports.read = (req, res) => {
+//     const slug = req.params.slug.toLowerCase();
+
+//     Tag.findOne({ slug }).exec((err, tag) => {
+//         if (err) {
+//             return res.status(400).json({
+//                 error: 'Tag not found'
+//             });
+//         }
+//         res.json(tag);
+//     });
+// };
+
+// read tag and blogs related to that
 exports.read = (req, res) => {
     const slug = req.params.slug.toLowerCase();
 
@@ -42,7 +57,20 @@ exports.read = (req, res) => {
                 error: 'Tag not found'
             });
         }
-        res.json(tag);
+        // res.json(tag);
+        Blog.find({ tags: tag })
+            .populate('categories', '_id name slug')
+            .populate('tags', '_id name slug')
+            .populate('postedBy', '_id name')
+            .select('_id title slug excerpt categories postedBy tags createdAt updatedAt')
+            .exec((err, data) => {
+                if (err) {
+                    return res.status(400).json({
+                        error: errorHandler(err)
+                    });
+                }
+                res.json({ tag: tag, blogs: data });
+            });
     });
 };
 
